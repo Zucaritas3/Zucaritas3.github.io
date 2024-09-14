3fe7b918cdef3a541e576453928bfa115cfb8966
Zucaritas3 <juancaballo30001@gmail.com>
2024-09-14 15:52:03 -0600

    Delete script1.js

--- script1.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('mirror');
-
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        video.srcObject = stream;
-    })
-    .catch(error => {
-        console.error('Error al acceder a la cámara:', error);
-    });
